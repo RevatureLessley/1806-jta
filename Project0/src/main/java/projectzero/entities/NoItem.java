c95128f39b4524bf1935a311db312c0cdf70cfe0
Nathan Edwards <nathan.w.edwards@outlook.com>
2018-06-18 22:20:50 -0500

    added Project0 with gradle with Vending Machine, Inventory, Item, StockItem

---
+++ Project0/src/main/java/projectzero/entities/NoItem.java
@@ -0,0 +1,12 @@
+package projectzero.entities;
+
+public class NoItem extends Item {
+
+    public NoItem(String name, int value, String description) {
+	this();
+    }
+
+    public NoItem() {
+	super("@", 0, "This is not an item.");
+    }
+}
---
+++ Project0/src/main/java/projectzero/entities/VendingMachine.java
@@ -0,0 +1,101 @@
+package projectzero.entities;
+
+import java.util.HashMap;
+
+import projectzero.entities.Inventory;
+import projectzero.entities.StockItem;
+import projectzero.entities.NoItem;
+
+public class VendingMachine {
+
+    private final String vendingMachineName;
+    private final String adminName;
+    private String message;
+    private Inventory inventory;
+
+    /**
+     * Instantiates a vending machine object.
+     *
+     * @param vendingMachineName vending machine name
+     * @param adminName vending machine owner's name
+     */
+    public VendingMachine(String vendingMachineName, String adminName) {
+	
+	this.vendingMachineName = vendingMachineName;
+	this.adminName = adminName;
+	this.message = "An empty vending machine.";
+	this.inventory = VendingMachine.buildEmptyInventory();
+    }
+
+    /**
+     * Instantiates a vending machine object.
+     *
+     * @param vendingMachineName vending machine name
+     * @param adminName vending machine owner's name
+     * @param message vending machine's welcome message
+     */
+    public VendingMachine(String vendingMachineName,
+			  String adminName,
+			  String message) {
+	this.vendingMachineName = vendingMachineName;
+	this.adminName = adminName;
+	this.message = message;
+	this.inventory = VendingMachine.buildEmptyInventory();
+    }
+
+    /**
+     *
+     *
+     */
+    public VendingMachine(String vendingMachineName,
+			  String adminName,
+			  Inventory inventory) {
+	this.vendingMachineName = vendingMachineName;
+	this.adminName = adminName;
+	this.inventory = inventory;
+    }
+    
+    /**
+     * Creates an empty inventory with selections A1-D5; A-D and 1-5.
+     *
+     * @return inventory an empty vending machine inventory
+     */
+    public static Inventory buildEmptyInventory() {
+
+	Inventory inventory = new Inventory();
+	String[] columns = {"A", "B", "C", "D"};
+	String[] rows = {"1", "2", "3", "4", "5"};
+
+	for (String column : columns) {
+	    for (String row: rows) {
+		String key = column + row;
+		Item noItem = new NoItem();
+		StockItem stockItem = new StockItem(noItem, 0);
+		inventory.put(key, stockItem);
+	    }
+	} 
+
+	return inventory;
+    }
+
+    /** 
+     * Return's the name of the vending machine.
+     *
+     * @return this.vendingMachineName the Vending Machine's name
+     */
+    public String getVendingMachineName() {
+
+	return this.vendingMachineName;
+    }
+
+    /**
+     * Returns the Admin's (Owner's) name of the vending machine.
+     *
+     * @return this.adminName the owner's name of the vending machine.
+     */
+    public String getAdminName() {
+
+	return this.adminName;
+    }
+    
+}
