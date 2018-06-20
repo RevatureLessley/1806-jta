b40ffda560ef301f819f8c323f6436f03bb2da6e
Nathan Edwards <nathan.w.edwards@outlook.com>
2018-06-20 16:38:46 -0500

    added Input.java

--- Project0/src/main/java/projectzero/utilities/Input.java
+++ Project0/src/main/java/projectzero/utilities/Input.java
@@ -2,6 +2,7 @@ package projectzero.utilities;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.lang.StringBuffer;
 
 public class Input {
     private final Scanner scanner;
@@ -17,20 +18,59 @@ public class Input {
     }
 
     public Scanner getScanner() {
-	return this.scanner();
-    }
-
-    public String getInput() {
-	this.add(scanner.next());
+	return this.scanner;
     }
     
     public ArrayList<String> getHistory() {
-	return this.history();
+	return this.history;
     }
-    
-    private void add() {
-	if (this.history.size() >= this.historyRecordCapacity || this.histIndexPointer >= this.historyRecordCapacity) {
 
+    public void printHistory() {
+	StringBuffer bf = new StringBuffer();
+	int index = this.history.size() == this.historyRecordCapacity ? this.histIndexPointer + 1 % this.history.size() : this.history.size() - 1;
+	for (int i = 0; i < this.history.size(); i++){
+	    index = index + i % this.history.size();
+	    System.out.println("\t\t" + this.history.get(i));
+	}
+	System.out.println("A review of the past " + this.history.size() + " input values.");
+    }
+
+    /**
+     * Add input to history array list. If the history array list exceeds the capacity move the index pointer to the beginning.
+     *
+     */
+    private void add(String input) {
+	if (this.history.size() >= this.historyRecordCapacity) {
+	    this.histIndexPointer = 0;
+	    this.history.set(this.histIndexPointer, input);
+	}
+	this.history.add(input);
+	this.histIndexPointer++;
+    }
+
+    public String next(){
+	String input = this.scanner.next();
+	this.add(input);
+	return input;
+    }
+
+    
+    public static void main(String[] args) {
+	Input input = new Input();
+	String userInput = "";
+
+	shell:
+	while ((userInput = input.next()) != "close") {
+	    switch(userInput) {
+		default:
+		    System.out.println(userInput);
+		    break;
+	        case "history":
+		    input.printHistory();
+		    break;
+	        case "exit":
+		    break shell;
+	    }
 	}
     }
 }
