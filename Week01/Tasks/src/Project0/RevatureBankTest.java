be4a1ec0e17901ca23e195962acaeb94d4f2d159
swilery <swilery@gmail.com>
2018-06-25 02:15:45 -0500

    JUnit

---
+++ Week01/Tasks/src/Project0/RevatureBankTest.java
@@ -0,0 +1,23 @@
+package Project0;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class RevatureBankTest {
+	
+	@Test
+	public void testAdminExists() {
+		assertTrue(RevatureBank.accountExists("admin"));
+	}
+	
+	@Test
+	public void testNoOtherAccount() {
+		assertFalse(RevatureBank.accountExists(""));
+	}
+	
+	@Test
+	public void testUnapprovedAccountsNotNull() {
+		assertNotNull(RevatureBank.getUnapprovedAccounts());
+	}
+
+}
