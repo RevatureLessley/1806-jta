548ebd5f969d4eb8219b408b01dfd025a98257bb
Vladimir B <vladimir.bukhalo@gmail.com>
2018-06-24 20:30:21 -0500

    Added JavaDocs, JUnit tests, Logging

--- Week01/Tasks/Project0/src/Driver.java
+++ Week01/Tasks/Project0/src/Driver.java
@@ -10,14 +10,26 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import org.apache.log4j.Logger;
+
+/**
+ * The Driver class is the class from which the application will run.
+ * @author Vladimir Bukhalo
+ *
+ */
 public class Driver {
+	final static Logger logger = Logger.getLogger(Driver.class);
 	
 	private static Scanner in;
 	private static Map<String, User> users;
 	private static Map<String, User> unAppUsers;
-	private static List<Map> list; 
+	private static List<Map<String, User>> list; 
 
-	public static void main(String[] args) {
+	/**
+	 * The main() method is responsible for initializing fields calling methods for the application to run.
+	 * @param args String array passed in if running application from the command line.
+	 */
+	public static void main(String[] args) {		
 		list = new ArrayList<>();
 		
 		File f = new File("users.ser");
@@ -25,8 +37,10 @@ public class Driver {
 		if(f.exists()) {
 			users = deserializeUsers().get(0);
 			unAppUsers = deserializeUsers().get(1);
+			logger.info("users.ser file found");
 		}
 		else {
+			logger.info("users.ser file not found");
 			Account adminAccount = new Account();
 			users = new HashMap<>();
 			User admin = new User("admin", "password", adminAccount,true,true);
@@ -43,6 +57,9 @@ public class Driver {
 		in.close();
 	}
 	
+	/**
+	 * The run() method will run the application in the console.
+	 */
 	public static void run() {	
 		welcome();
 		
@@ -105,7 +122,6 @@ public class Driver {
 						System.out.println("To approve users, type 'Approve', OR " 
 								+  "Select from the following commands");		
 						System.out.println("'Deposit' 'Withdraw' 'Balance' 'Logout'");
-						System.out.println();
 						
 						String command = in.nextLine().toLowerCase();
 						activeAdmin = adminMenu(command, admin);
@@ -136,57 +152,94 @@ public class Driver {
 	
 		
 
-	
+	/**
+	 * The welcome() method will print a welcome message when the application starts.
+	 */
 	public static void welcome() {
 		System.out.println("Welcome");
 		System.out.println("To login type 'login'. To register type 'register'" );
 	}
 	
+	/**
+	 * The register() method will register a new user.
+	 */
 	public static void register() {
 		Account acc = new Account();
 		
 		System.out.print("Please enter username: ");
 		String uName = in.nextLine();
+		
+		while(users.containsKey(uName)) {
+			System.out.println("Sorry username already exists.");
+			System.out.println();
+			System.out.print("Please enter new username: ");
+			uName = in.nextLine();
+		}
+		
 		System.out.print("Please enter password: ");
 		String pass = in.nextLine();
+		System.out.println();
 		
 		User u = new User(uName,pass,acc,false,false);
 		users.put(u.getUserName(),u);
 		unAppUsers.put(uName, u);
+		logger.info(u.getUserName() + " registered for new account");
 		
 		System.out.println("Thank you for registering.");
 		System.out.println("An administrator will approve your account");
 		System.out.println();		
 	}
 
-	
+	/**
+	 * The userMenu() method will allow a user to deposit, withdraw, get balance and logout
+	 * of their account.
+	 * @param command The action taken by the user.
+	 * @param u The user for which action is to be performed.
+	 * @return Returns false if the user logs out, otherwise returns true.
+	 */
 	public static boolean userMenu(String command, User u) {
 		boolean active = true;
 		
-		
-		if(command.equals("deposit")) {
-			System.out.print("How much would you like to deposit? $");
-			int amount = Integer.parseInt(in.nextLine());
-			System.out.println("Balance after deposit $" + u.getAccount().deposit(amount));
+		try {
+			if(command.equals("deposit")) {
+				System.out.print("How much would you like to deposit? $");
+				int amount = Integer.parseInt(in.nextLine());
+				System.out.println("Balance after deposit $" + u.getAccount().deposit(amount));
+				logger.info(u.getUserName() + " deposited $" + amount);
+			}
+			else if(command.equals("withdraw")) {
+				System.out.print("How much would you like to withdraw? $");
+				int amount = Integer.parseInt(in.nextLine());
+				System.out.println("Balance after withdrawal $" + u.getAccount().withdrawal(amount));
+				logger.info(u.getUserName() + " withdrew $" + amount);
+			}
+			else if(command.equals("balance")) {
+				System.out.println("Current balance $" + u.getUserBalance());
+			}
+			else if(command.equals("logout")) {
+				active = false;
+				logger.info(u.getUserName() + " logged out");
+			}
+			else {
+				System.out.println("Not a valid command");
+			}
 		}
-		else if(command.equals("withdraw")) {
-			System.out.print("How much would you like to withdraw? $");
-			int amount = Integer.parseInt(in.nextLine());
-			System.out.println("Balance after withdrawal $" + u.getAccount().withdrawal(amount));
-		}
-		else if(command.equals("balance")) {
-			System.out.println("Current balance $" + u.getUserBalance());
-		}
-		else if(command.equals("logout")) {
-			active = false;
-		}
-		else {
-			System.out.println("Not a valid command");
+		catch(NumberFormatException e) {
+			System.out.println("Invalid input. Please try again.");
+			logger.error(u.getUserName() + " entered invalid amount");
 		}
 		System.out.println();
 		return active;
 	}
 	
+	/**
+	 * The adminMenu is the menu presented to an administrator. In addition to providing 
+	 * options to deposit, withdraw and get balance, administrators can also choose to approve
+	 * user accounts.
+	 * @param command The command chosen by the administrator.
+	 * @param u The user for which the action is to be performed.
+	 * @return Returns false if the user logs out, otherwise returns true.
+	 */
 	public static boolean adminMenu(String command, User u) {
 		boolean active = true;
 		
@@ -204,6 +257,7 @@ public class Driver {
 				unAppUsers.remove(user);
 				System.out.println(user + " Approved");
 				System.out.println();
+				logger.info(user + " approved by administrator");
 			}								
 		}
 		else if(command.equals("approve") && unAppUsers.isEmpty()) {
@@ -215,10 +269,12 @@ public class Driver {
 		}
 		return active;
 		
-	}
+	}	
 	
-	
-	
+	/**
+	 * The serialize() method will serialize user data to a 'users.ser' file for data 
+	 * persistence.
+	 */
 	public static void serialize() {
 		try {
 			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("users.ser"));
@@ -229,12 +285,16 @@ public class Driver {
 		}
 	}
 	
-//	@SuppressWarnings("unchecked")
-	public static List<Map> deserializeUsers() {
-		List<Map> users = new ArrayList<>();
+	/**
+	 * The deserializeUsers() will deserialize the 'users.ser' file and load user information
+	 * from the previous application run.
+	 * @return Returns a list of maps which hold information about users and unapproved users.
+	 */
+	public static List<Map<String, User>> deserializeUsers() {
+		List<Map<String, User>> users = new ArrayList<>();
 		try {
 			ObjectInputStream ois = new ObjectInputStream(new FileInputStream("users.ser"));
-			users = (List<Map>)ois.readObject();
+			users = (List<Map<String, User>>)ois.readObject();
 		}
 		catch(Exception e) {
 			e.printStackTrace();
--- Week01/Tasks/Project0/src/User.java
+++ Week01/Tasks/Project0/src/User.java
@@ -1,5 +1,10 @@
 import java.io.Serializable;
 
+/**
+ * The User class contains a blueprint for a User.
+ * @author Vladimir Bukhalo
+ *
+ */
 public class User implements Serializable {
 	
 	private String userName;
@@ -8,6 +13,14 @@ public class User implements Serializable {
 	private boolean isApproved;
 	private boolean isAdmin;
 	
+	/**
+	 * The User() constructor creates an instance of a User object.
+	 * @param userName The user's username.
+	 * @param password The user's password.
+	 * @param account The account associated with the user.
+	 * @param isApproved Indicates if the user is an approved user. Default is false.
+	 * @param isAdmin Indicates if the user is an administrator. Default is false.
+	 */
 	public User(String userName, String password, Account account, boolean isApproved, boolean isAdmin) {
 		this.userName = userName;
 		this.password = password;
