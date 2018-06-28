95b4c77c18a4578905da780f5f147011d6e21233
Ryan Lessley <ryan.lessley@revature.com>
2018-06-28 13:43:17 -0500

    SQL

---
+++ Week02/_InClassExamples/180618_JDBC/src/main/java/com/revature/beans/JobClass.java
@@ -0,0 +1,28 @@
+package com.revature.beans;
+
+public class JobClass {
+	private Integer id;
+	private String name;
+	
+	
+	
+	public JobClass(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
