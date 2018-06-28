469807fd6057acc5871c76c3650f3ad4735d119b
Ryan Lessley <ryan.lessley@revature.com>
2018-06-28 13:43:17 -0500

    SQL

--- Week02/_InClassExamples/180618_JDBC/src/main/java/com/revature/service/NpcService.java
+++ Week02/_InClassExamples/180618_JDBC/src/main/java/com/revature/service/NpcService.java
@@ -1,15 +1,36 @@
 package com.revature.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.revature.beans.JobClass;
 import com.revature.beans.Npc;
+import com.revature.dao.JobClassDao;
+import com.revature.dao.JobClassDaoImpl;
 import com.revature.dao.NpcDao;
 import com.revature.dao.NpcDaoImpl;
 
 public class NpcService {
 	public List<Npc> getAllNpcsWithClass(){
 		NpcDao nd = new NpcDaoImpl();
-		return nd.selectAllNpc();
+		JobClassDao jcd = new JobClassDaoImpl();
+		
+		List<Npc> npcs = nd.selectAllNpc();
+		List<JobClass> jcs = jcd.getAllJobs();
+		
+		
+		
+		for(Npc n: npcs){
+			for(JobClass jc: jcs){
+				if(n.getJobClass().equals(jc.getId())){
+					n.setJobClassString(jc.getName());
+				}
+			}
+		}
+		
+		
+		
+		return npcs;
 		
 		//getAllNpc (Which wouldnt have jobStringClass set
 		//getAllClasses
