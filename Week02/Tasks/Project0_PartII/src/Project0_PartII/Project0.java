75004177ffefe8d644bad4328de5fb0ba4886b87
swilery <swilery@gmail.com>
2018-06-30 16:42:34 -0500

    Connection established

--- Week02/Tasks/Project0_PartII/src/Project0_PartII/Project0.java
+++ Week02/Tasks/Project0_PartII/src/Project0_PartII/Project0.java
@@ -1,29 +1,36 @@
 package Project0_PartII;
 
+import java.sql.*;
+import Project0_PartII.RevatureDatabase.DatabaseConnection;
+
 public class Project0 implements ConsoleReference, LogReference{
 	/**
 	 * main() runs the RevatureBank application.
 	 * @param args unused
 	 */
 	public static void main(String args[]){
-		logger.debug("Project0/Project0.java: " + 
-	                 "Application Started, entered main().");
+//		logger.debug("Project0/Project0.java: " + 
+//	                 "Application Started, entered main().");
+//		
+//  		if(console == null) {
+//   			System.err.print("Thank you for choosing RevatureBank. Please " + 
+//   							 "note that this is a console application. " + 
+//   					         "Please open your command prompt.");
+//			logger.info("Project0/Project0.java: " + 
+//   					    "User tried to run application in an IDE.");
+//  		}
+//
+//  		else {
+//   			RevatureBank bank = RevatureBank.entrance;
+//   			bank.enter();
+//   			bank.exit();
+//  		}
+//  		
+//  		logger.debug("Project0/Project0.java: " + 
+//  		             "Application Ended, exiting main().");
 		
-  		if(console == null) {
-   			System.err.print("Thank you for choosing RevatureBank. Please " + 
-   							 "note that this is a console application. " + 
-   					         "Please open your command prompt.");
-			logger.info("Project0/Project0.java: " + 
-   					    "User tried to run application in an IDE.");
-  		}
-
-  		else {
-   			RevatureBank bank = RevatureBank.entrance;
-   			bank.enter();
-   			bank.exit();
-  		}
-  		
-  		logger.debug("Project0/Project0.java: " + 
-  		             "Application Ended, exiting main().");
+		Connection con = DatabaseConnection.connection;
+		System.out.println("We have access to the database.");
+		DatabaseConnection.close(con);
   	}
 }
---
+++ Week02/Tasks/Project0_PartII/src/Project0_PartII/RevatureDatabase/DatabaseConnection.java
@@ -0,0 +1,75 @@
+package Project0_PartII.RevatureDatabase;
+
+import java.io.*;
+import java.sql.*;
+
+public class DatabaseConnection {
+	public static Connection connection = connect();
+	
+	private static Connection connect() {
+		try {
+			String args[] = System.getenv("DBARGS").split(";");
+			Class.forName(args[0]);
+			connection = 
+					DriverManager.getConnection(args[1], args[2], args[3]);
+		}
+		
+		catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} 
+		
+		return connection;
+	}
+	
+	public static void close(Statement resource){
+		if(resource!=null) {
+			try{
+				resource.close();
+			}
+			
+			catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void close(ResultSet resource){
+		if(resource!=null) {
+			try{
+				resource.close();
+			}
+			
+			catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void close(FileInputStream resource){
+		if(resource!=null) {
+			try{
+				resource.close();
+			}
+			
+			catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void close(Connection resource){
+		if(resource!=null){
+			try{
+				resource.close();
+			}
+			
+			catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
