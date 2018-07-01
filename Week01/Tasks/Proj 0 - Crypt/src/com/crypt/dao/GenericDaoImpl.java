8aa2db45b75d2e9a15cba35382b9fc611f24bf26
Austin Davis <audavis12@gmail.com>
2018-06-30 22:02:21 -0500

    Transfer2

--- Week01/Tasks/Proj 0 - Crypt/src/com/crypt/dao/GenericDaoImpl.java
+++ Week01/Tasks/Proj 0 - Crypt/src/com/crypt/dao/GenericDaoImpl.java
@@ -1,14 +1,22 @@
 package com.crypt.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
-public abstract class GenericDaoImpl<T, K> implements GenericDao<T, K> {
+import com.crypt.util.Connections;
+import static com.crypt.util.CloseStreams.*;
 
+public abstract class GenericDaoImpl<T, K> implements GenericDao<T, K> {
+	
 	@Override
 	public List<T> selectAll() {
 		Statement stmt = null; // Simple SQL query to be executed
 		ResultSet rs = null; //Object that holds query results
-		List<Npc> npcs = new ArrayList<>();
+		List<T> npcs = new ArrayList<>();
 		
 		try(Connection conn = Connections.getConnection()){
 			String sql = "SELECT * FROM npc";
@@ -19,14 +27,14 @@ public abstract class GenericDaoImpl<T, K> implements GenericDao<T, K> {
 			
 
 			while(rs.next()){
-				Npc npc = new Npc(
+				T t = new T(
 						rs.getInt(1),
 						rs.getString("npc_name"),
 						rs.getInt(3),
 						rs.getInt(4),
 						rs.getInt(5)
 						);
-				npcs.add(npc);
+				npcs.add(t);
 			}
 			
 		}catch(SQLException e){
