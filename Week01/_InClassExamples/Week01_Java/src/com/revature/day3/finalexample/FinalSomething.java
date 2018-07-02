e9cf60849ba212b1aa8aa051f0f1bc48ee21ca18
Robert Cheng <chengrob000@gmail.com>
2018-07-02 17:54:07 -0500

    Removing files

--- Week01/_InClassExamples/Week01_Java/src/com/revature/day3/finalexample/FinalSomething.java
+++
@@ -1,7 +0,0 @@
-package com.revature.day3.finalexample;
-
-//NOTE: The only non access modifiers a class can have is final and abstract.
-//A final class cannot be inherited/extended
-public final class FinalSomething {
-	
-}
--- Week02/_InClassExamples/180618_JDBC/src/main/java/com/revature/dao/JobClassDao.java
+++
@@ -1,9 +0,0 @@
-package com.revature.dao;
-
-import java.util.List;
-
-import com.revature.beans.JobClass;
-
-public interface JobClassDao {
-	public List<JobClass> getAllJobs();
-}
