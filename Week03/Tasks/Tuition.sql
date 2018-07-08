949d1c9538f0ef86b4d3a17219938a8a85c966e5
Andrew Hwang <ahwang1995@gmail.com>
2018-07-07 23:09:40 -0500

    created basic insert stored procedures and default table values

--- Week03/Tasks/Tuition.sql
+++ Week03/Tasks/Tuition.sql
@@ -47,8 +47,8 @@ CREATE TABLE Event (
     event_name varchar2(100) NOT NULL,
     event_type_id number(6) NOT NULL,
     grade_format number(1) NOT NULL, --0 for presentation 1 for grade
-    cutoff_grade number(6) NOT NULL, --defaults from event type
-    event_cost number(6),
+    cutoff_grade number(6,2) NOT NULL, --defaults to 70 if not specified (can be changed)
+    event_cost number(6,2),     --can be changed
     
     CONSTRAINT fk_event_type_id FOREIGN KEY (event_type_id) REFERENCES EventType(event_type_id)
     
@@ -57,13 +57,12 @@ CREATE TABLE Event (
 CREATE TABLE RForm (
     rform_id number(6) PRIMARY KEY,
     emp_id number(6) NOT NULL,
-    form_date date,
-    deadline_date date,
+    rform_date date NOT NULL,
+    deadline_date date NOT NULL,
     place varchar2(100) NOT NULL,
     info varchar2(1000),
-    event_id number(6),
-    reimbursement_amount number(6), --default from event, can be changed
-    
+    event_id number(6) NOT NULL,
+    prop_reim number(6,2), --value from event is used, this is proposed value
     justification varchar2(500),
     --event related attatchment blobs id
     time_missed number(6), --hours
@@ -79,8 +78,12 @@ CREATE TABLE Approval (
     rform_id number(6),
     is_app number(1),
     app_lvl number(1),
+    approver number(6),
+    requester number(6),
     
-    CONSTRAINT fk_rform_id FOREIGN KEY (rform_id) REFERENCES RForm(rform_id)
+    CONSTRAINT fk_rform_id FOREIGN KEY (rform_id) REFERENCES RForm(rform_id),
+    CONSTRAINT fk_approver FOREIGN KEY (approver) REFERENCES Employee(emp_id),
+    CONSTRAINT fk_requester FOREIGN KEY (requester) REFERENCES Employee(emp_id)
 );
 
 --Sequencers and their triggers
@@ -153,6 +156,45 @@ BEGIN
 END;
 /
 
+--Employee types table
+INSERT INTO EmployeeType (emp_type_id,emp_type)
+VALUES(0,'Regular');
+INSERT INTO EmployeeType (emp_type_id,emp_type)
+VALUES(1,'Supervisor');
+INSERT INTO EmployeeType (emp_type_id,emp_type)
+VALUES(2,'Head');
+INSERT INTO EmployeeType (emp_type_id,emp_type)
+VALUES(3,'Executive');
+
+--Department table
+INSERT INTO Department(dep_id,dep_name,dep_head_id)
+VALUES(0,'Executive',0);
+INSERT INTO Department(dep_id,dep_name,dep_head_id)
+VALUES(1,'Benefits',1);
+INSERT INTO Department(dep_id,dep_name,dep_head_id)
+VALUES(2,'Production',2);
+INSERT INTO Department(dep_id,dep_name,dep_head_id)
+VALUES(3,'Finance',3);
+INSERT INTO Department(dep_id,dep_name,dep_head_id)
+VALUES(4,'Marketing',4);
+
+--Department heads
+INSERT INTO Employee (emp_id,username,pass_word,f_name,l_name,
+                            dep_id,bal,pending_reim,awarded_reim,emp_type_id,auth_lvl)
+VALUES(0,'executive','executive','Boss','Man',0,0,0,0,3,2);
+INSERT INTO Employee (emp_id,username,pass_word,f_name,l_name,
+                            dep_id,bal,pending_reim,awarded_reim,emp_type_id,auth_lvl)
+VALUES(1,'benefits','benefits','Bobert','Bobson',1,0,0,0,2,1);
+INSERT INTO Employee (emp_id,username,pass_word,f_name,l_name,
+                            dep_id,bal,pending_reim,awarded_reim,emp_type_id,auth_lvl)
+VALUES(2,'production','production','Tommy','Droptables',2,0,0,0,2,1);
+INSERT INTO Employee (emp_id,username,pass_word,f_name,l_name,
+                            dep_id,bal,pending_reim,awarded_reim,emp_type_id,auth_lvl)
+VALUES(3,'finance','finance','Johnny','Tsunami',3,0,0,0,2,1);
+INSERT INTO Employee (emp_id,username,pass_word,f_name,l_name,
+                            dep_id,bal,pending_reim,awarded_reim,emp_type_id,auth_lvl)
+VALUES(4,'marketing','marketing','Timmy','Turner',4,0,0,0,2,1);
+
 --Event types table
 INSERT INTO EventType(event_type_id,event_type,percent_reimb)
 VALUES(0,'University Course',0.8);
@@ -167,25 +209,71 @@ VALUES(4,'Technical Training',0.9);
 INSERT INTO EventType(event_type_id,event_type,percent_reimb)
 VALUES(5,'Other',0.3);
 
+--insert employee
 CREATE OR REPLACE PROCEDURE insertNewEmployee(userN IN varchar2,
                                             userP IN varchar2,
                                             firstN IN varchar2,
                                             lastN IN varchar2,
                                             dirS IN varchar2,
                                             depId IN number,
-                                            pending_reim IN number,
-                                            awarded_reim IN number,
+                                            pendingR IN number,
+                                            awardedR IN number,
                                             empT in number)
 IS
 BEGIN
     INSERT INTO Employee (username,pass_word,f_name,l_name,dir_sup_id,
                             dep_id,bal,pending_reim,awarded_reim,emp_type_id,auth_lvl)
-    VALUES(userN,userP,firstN,lastN,dirS,depId,0,empT,0);
+    VALUES(userN,userP,firstN,lastN,dirS,depId,0,0,0,empT,0);
     commit;
 END;
 /
 
-CREATE OR REPLACE PROCEDURE insertNewEvent(eventN
-                                        )
+--insert event
+CREATE OR REPLACE PROCEDURE insertNewEvent(eventN IN varchar2,
+                                        eventT_Id IN varchar2,
+                                        gradeF IN number,
+                                        cutoffG in number,
+                                        eventC IN number)
+IS
+BEGIN
+    INSERT INTO Event (event_name,event_type_id,grade_format,cutoff_grade,event_cost)
+    VALUES(eventN,eventT_Id,gradeF,cutoffG,eventC);
+    commit;
+END;
+/
+
+--insert rform
+CREATE OR REPLACE PROCEDURE insertNewRForm(empId IN number,
+                                        rformD IN date,
+                                        deadlineD IN date,
+                                        pl IN varchar2,
+                                        inf IN varchar2,
+                                        eventId IN number,
+                                        propR IN number,
+                                        just IN varchar2,
+                                        timeM IN number,
+                                        formC IN number,
+                                        applvl IN number)
+IS
+BEGIN
+    INSERT INTO RForm (emp_id,rform_date,deadline_date,place,info,event_id,prop_reim,
+                        justification,time_missed,form_closed,app_lvl)
+    VALUES(empId,rformD,deadlineD,pl,inf,eventId,propR,just,timeM,formC,applvl);
+    commit;
+END;
+/
+
+CREATE OR REPLACE PROCEDURE insertNewApproval(rformid IN number,
+                                            isapp IN number,
+                                            applvl IN number,
+                                            appr IN number,
+                                            req IN number)
+IS
+BEGIN
+    INSERT INTO Approval(rform_id,is_app,app_lvl,approver,requester)
+    VALUES(rformid,isapp,applvl,appr,req);
+    commit;
+END;
+/
 commit;
 
