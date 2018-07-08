8caa9d74f9d90a4f2b57e2d01f580977e8183881
Andrew Hwang <ahwang1995@gmail.com>
2018-07-07 20:51:10 -0500

    sequencers, triggers, and started stored procedures

--- Week03/Tasks/Tuition.sql
+++ Week03/Tasks/Tuition.sql
@@ -6,6 +6,7 @@ DROP TABLE Event CASCADE CONSTRAINTS;
 DROP TABLE EventType CASCADE CONSTRAINTS;
 DROP TABLE Approval CASCADE CONSTRAINTS;
 
+--Tables
 CREATE TABLE EmployeeType (
     emp_type_id number(6) PRIMARY KEY,
     emp_type varchar(100) NOT NULL
@@ -13,7 +14,8 @@ CREATE TABLE EmployeeType (
 
 CREATE TABLE Department (
     dep_id number(6) PRIMARY KEY,
-    dep_name varchar2(100)
+    dep_name varchar2(100),
+    dep_head_id number(6)
 );
 
 CREATE TABLE Employee (
@@ -24,7 +26,9 @@ CREATE TABLE Employee (
     l_name varchar2(100) NOT NULL,
     dir_sup_id number(6),
     dep_id number(6) NOT NULL,
-    bal number(8,2) NOT NULL,
+    bal number(6,2) NOT NULL,
+    pending_reim number(6,2) NOT NULL,
+    awarded_reim number(6,2) NOT NULL,
     emp_type_id number(6) NOT NULL,
     auth_lvl number(1) NOT NULL,
     
@@ -35,7 +39,7 @@ CREATE TABLE Employee (
 CREATE TABLE EventType(
     event_type_id number(6) PRIMARY KEY,
     event_type varchar2(100),
-    percent_reimb number(6)
+    percent_reimb number(6,2)
 );
 
 CREATE TABLE Event (
@@ -51,7 +55,7 @@ CREATE TABLE Event (
 );
 
 CREATE TABLE RForm (
-    form_id number(6) PRIMARY KEY,
+    rform_id number(6) PRIMARY KEY,
     emp_id number(6) NOT NULL,
     form_date date,
     deadline_date date,
@@ -72,12 +76,116 @@ CREATE TABLE RForm (
 
 CREATE TABLE Approval (
     app_id number(6) PRIMARY KEY,
-    form_id number(6),
+    rform_id number(6),
     is_app number(1),
     app_lvl number(1),
     
-    CONSTRAINT fk_form_id FOREIGN KEY (form_id) REFERENCES RForm(form_id)
+    CONSTRAINT fk_rform_id FOREIGN KEY (rform_id) REFERENCES RForm(rform_id)
 );
 
+--Sequencers and their triggers
+
+--Create new employee id
+DROP SEQUENCE empid_seq;
+CREATE SEQUENCE empid_seq
+    START WITH 100
+    INCREMENT BY 1;
+/
+
+CREATE OR REPLACE TRIGGER empid_seq_trigger
+BEFORE INSERT ON Employee
+FOR EACH ROW 
+BEGIN 
+    IF :new.emp_id IS NULL THEN
+        SELECT empid_seq.NEXTVAL INTO :new.emp_id FROM dual;
+    END IF;
+END;
+/
+
+--Create new rform id
+DROP SEQUENCE rformid_seq;
+CREATE SEQUENCE rformid_seq
+    START WITH 100
+    INCREMENT BY 1;
+/
+
+CREATE OR REPLACE TRIGGER rformid_seq_trigger
+BEFORE INSERT ON RForm
+FOR EACH ROW 
+BEGIN 
+    IF :new.rform_id IS NULL THEN
+        SELECT rformid_seq.NEXTVAL INTO :new.rform_id FROM dual;
+    END IF;
+END;
+/
+
+--Create new event id
+DROP SEQUENCE eventid_seq;
+CREATE SEQUENCE eventid_seq
+    START WITH 100
+    INCREMENT BY 1;
+/
+
+CREATE OR REPLACE TRIGGER eventid_seq_trigger
+BEFORE INSERT ON Event
+FOR EACH ROW 
+BEGIN 
+    IF :new.event_id IS NULL THEN
+        SELECT eventid_seq.NEXTVAL INTO :new.event_id FROM dual;
+    END IF;
+END;
+/
+
+--Create new approval id
+DROP SEQUENCE appid_seq;
+CREATE SEQUENCE appid_seq
+    START WITH 100
+    INCREMENT BY 1;
+/
+
+CREATE OR REPLACE TRIGGER appid_seq_trigger
+BEFORE INSERT ON Approval
+FOR EACH ROW 
+BEGIN 
+    IF :new.app_id IS NULL THEN
+        SELECT appid_seq.NEXTVAL INTO :new.app_id FROM dual;
+    END IF;
+END;
+/
+
+--Event types table
+INSERT INTO EventType(event_type_id,event_type,percent_reimb)
+VALUES(0,'University Course',0.8);
+INSERT INTO EventType(event_type_id,event_type,percent_reimb)
+VALUES(1,'Seminar',0.6);
+INSERT INTO EventType(event_type_id,event_type,percent_reimb)
+VALUES(2,'Certification Preparation Class',0.75);
+INSERT INTO EventType(event_type_id,event_type,percent_reimb)
+VALUES(3,'Certification',1.0);
+INSERT INTO EventType(event_type_id,event_type,percent_reimb)
+VALUES(4,'Technical Training',0.9);
+INSERT INTO EventType(event_type_id,event_type,percent_reimb)
+VALUES(5,'Other',0.3);
+
+CREATE OR REPLACE PROCEDURE insertNewEmployee(userN IN varchar2,
+                                            userP IN varchar2,
+                                            firstN IN varchar2,
+                                            lastN IN varchar2,
+                                            dirS IN varchar2,
+                                            depId IN number,
+                                            pending_reim IN number,
+                                            awarded_reim IN number,
+                                            empT in number)
+IS
+BEGIN
+    INSERT INTO Employee (username,pass_word,f_name,l_name,dir_sup_id,
+                            dep_id,bal,pending_reim,awarded_reim,emp_type_id,auth_lvl)
+    VALUES(userN,userP,firstN,lastN,dirS,depId,0,empT,0);
+    commit;
+END;
+/
+
+CREATE OR REPLACE PROCEDURE insertNewEvent(eventN
+                                        )
 commit;
 
