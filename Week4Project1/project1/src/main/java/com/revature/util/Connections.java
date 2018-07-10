a50d7eef49c88b31b2e54cbf3b71b1a7bf4baeae
Logan Brewer <logan211@live.com>
2018-07-10 15:59:13 -0500

    some css and initial java setup

---
+++ Week4Project1/project1/src/main/java/com/revature/util/Connections.java
@@ -0,0 +1,47 @@
+package com.revature.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class Connections 
+{
+	private static Connection conn = null;
+	private final static String FILE_NAME = "dbprops.properties";
+	private static Properties prop = null;
+	
+	public static Connection getConnection()
+	{
+		try
+		{			
+			prop = new Properties();
+			prop.load(new FileInputStream(FILE_NAME));
+			
+			Class.forName(prop.getProperty("class"));
+			conn = DriverManager.getConnection(
+					prop.getProperty("url"),
+					prop.getProperty("username"),
+					prop.getProperty("password")
+					);
+			
+		} 
+		catch ( SQLException e1 ) 
+		{
+			e1.printStackTrace();
+		}
+		catch ( ClassNotFoundException e )
+		{
+			e.printStackTrace();
+		}
+		catch ( IOException e2 )
+		{
+			e2.printStackTrace();
+		}
+		return conn;
+	}
+	
+}
