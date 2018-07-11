56c97ee94e680b969c666d1c9b08af59e33d13e8
Andrew Hwang <ahwang1995@gmail.com>
2018-07-10 22:10:23 -0500

    choose file on front end and store locally

---
+++ Week03/Tasks/src/main/java/servlets/UploadServlet.java
@@ -0,0 +1,59 @@
+package servlets;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+/**
+ * Servlet implementation class UploadServlet
+ */
+public class UploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UploadServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletFileUpload sf = new ServletFileUpload(new DiskFileItemFactory());
+		try {
+			List<FileItem> files = sf.parseRequest(request);
+			for(FileItem item: files) {
+				item.write(new File("/Users/auhwang/Documents/Revature/Repo/Week03/Tasks/testfileupload/" +
+								item.getName()));
+			}
+			System.out.println("file uploaded");
+		} catch (FileUploadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
