3bcd86363b31905342d2999e6551763209ceca8d
SalaraElris <SalaraElris@gmail.com>
2018-07-11 09:19:45 -0500

    general update

---
+++ Project1/src/main/java/project1/Connections.java
@@ -0,0 +1,31 @@
+package project1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Connections {
+	
+	private static Connection conn = null;
+	
+	public static Connection getConnection() {
+		try {
+			String props[] = System.getenv("DBProps").split(";");
+			
+			Class.forName(props[0]);
+			conn = DriverManager.getConnection(props[1], props[2], props[3]);
+		}
+		catch(SQLException e) 
+		{
+			e.printStackTrace();
+			System.out.println("DATABSE CONNECTION FAILED.");
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return conn;
+		
+	}
+
+}
