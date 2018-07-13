e8a92ea6bda784828fe5c43f1d4db2deecdea7c7
Chris Parsons <chris@cparsonsdesign.com>
2018-07-13 10:57:33 -0500

    cosmetic changes mostly. Modified the HTML to read a it easier. Changed the while loop to use 'else if' statements instead of 'ifs' and 'continues'. ( Old habit from school. Was causing an infinite while loop with incrementor at the bottom of the loop) clear() is still being a finicky mistress :(

--- Week04/Tasks/FizzBuzz.js
+++ Week04/Tasks/FizzBuzz.js
@@ -1,33 +1,31 @@
 function fizzBuzz()
 	{
-	x = Number(document.getElementById('min').value);
-	y = Number(document.getElementById('max').value);
-		
-	while (x <= y) {
-		  
-		  if ( x % 3 === 0 && x % 5 === 0 ){
-			document.getElementById('para1').innerHTML += x + 'FIZZBUZZ1<br>';
-		    continue;
-		  }
-		  
-		  if( x % 3 === 0 ){
-			  document.getElementById('para1').innerHTML += x + 'FIZZ<br>';
-		    continue;
-		  }
-		  
-		  if( x % 5 === 0 ) {
-			document.getElementById('para1').innerHTML += x + 'BUZZ!<br>';
-		    continue;
-		  }
-		  
-		  document.getElementById('para1').innerHTML += x + '<br>';
-			console.log(x.toString());
-			x++;
-		  
+		x = Number(document.getElementById('min').value);
+		y = Number(document.getElementById('max').value);
+			
+		while (x <= y) 
+		{ 
+				
+				if( x % 3 == 0 && x % 5 == 0 )
+				{
+					document.getElementById('para1').innerHTML += x + ' <b>FIZZBUZZ!</b><br>';
+				}
+				else if( x % 3 == 0 )
+				{
+				  document.getElementById('para1').innerHTML += x + ' <b>FIZZ</b><br>';
+				}
+				else if( x % 5 == 0 ) 
+				{
+					document.getElementById('para1').innerHTML += x + ' <b>BUZZ!</b><br>';
+				}
+				document.getElementById('para1').innerHTML += x + '<br>';
+				console.log(x.toString());
+				x++;
 		}
 	}
 
 function clear()
 {
-	 document.getElementById('para1').innerHTML = " ";
+	// Not entirely sure why this won't work.
+	document.getElementById('para1').innerHTML = ' ';
 }
