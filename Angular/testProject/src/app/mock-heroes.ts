d2826dc1db43fc4860decaa391e8bf837aae6f85
SalaraElris <SalaraElris@gmail.com>
2018-07-13 13:53:10 -0500

    general safety update

---
+++ Angular/testProject/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Doom'},
+    { id: 12, name: 'Veritesa'},
+    { id: 13, name: 'Arctic'},
+    { id: 14, name: 'Catistrophia'},
+    { id: 15, name: 'Ultimus'},
+    { id: 16, name: 'Explosivo'},
+    { id: 17, name: 'Reborn'},
+    { id: 18, name: 'Whisp'},
+    { id: 19, name: 'Zenith'},
+    { id: 20, name: 'Aerth'}
+];
