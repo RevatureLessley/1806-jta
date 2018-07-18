a1e53e6d2bb6274b91d8ead513c838d501381c44
ausmo42 <ausmo42@gmail.com>
2018-07-18 14:30:56 -0500

    continue notification display

--- Week03/Tasks/Project1/src/main/webapp/resources/js/messagebuilder.js
+++ Week03/Tasks/Project1/src/main/webapp/resources/js/messagebuilder.js
@@ -2,6 +2,9 @@
 function createMessages(messages, target){
 	
 	console.log(messages);
+	messageArea = document.createElement("div");
+	messageArea.setAttribute("class","row justify-content-md-center");
+	document.querySelector(target).appendChild(messageArea);
 	
 	var link = document.querySelector('link[rel="import"][href="../template/message.html"]');
 	var template = link.import.querySelector("template");
@@ -12,7 +15,8 @@ function createMessages(messages, target){
 		tclone.getElementById("messageHead").innerHTML = messages[m]["sourceName"]
 		tclone.getElementById("message").innerHTML = messages[m]["notification"]["message"]
 		tclone.getElementById("messageDate").innerHTML = messages[m]["dateString"]
-	    document.querySelector(target).appendChild(tclone);
+	   
+		messageArea.appendChild(tclone);
 	}
 	
 }
