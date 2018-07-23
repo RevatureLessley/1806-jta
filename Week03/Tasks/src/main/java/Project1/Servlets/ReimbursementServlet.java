d3161c209178126b83e648e7e76706c37e4f0c38
swilery <swilery@gmail.com>
2018-07-22 19:22:22 -0500

    405 LOL

--- Week03/Tasks/src/main/java/Project1/Servlets/ReimbursementServlet.java
+++ Week03/Tasks/src/main/java/Project1/Servlets/ReimbursementServlet.java
@@ -32,82 +32,96 @@ public class ReimbursementServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException,
 												 IOException {
-		RequestDispatcher rd  = request.getRequestDispatcher("user/index.html");
+		RequestDispatcher rd  = 
+				request.getRequestDispatcher("user/index.html");
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		HttpSession session = request.getSession();
 		Employee employee = (Employee)session.getAttribute("employee");
-		System.out.println(employee);
 		String datetime = request.getParameter("datetime");
-		SimpleDateFormat sdf = null;
-		Timestamp eventTime = null;
 		
-		try {
-		    sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
-			eventTime = new Timestamp(sdf.parse(datetime).getTime());
-		} 
-		
-		catch (ParseException chrome) {
-			
-			try {
-				sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-				eventTime = new Timestamp(sdf.parse(datetime).getTime());
-			} 
-			
-			catch (ParseException pe) {
-				pe.printStackTrace();
-			}
-		}
-		
-		Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-		Calendar cal = Calendar.getInstance();
-	    cal.setTimeInMillis(currentTime.getTime());
-	    cal.add(Calendar.DAY_OF_MONTH, 7);
-	    Timestamp futureTime = new Timestamp(cal.getTime().getTime());
-
-	    if (futureTime.compareTo(eventTime) > 0) {
-	    	rd.include(request, response);
+		if(datetime == null) {
+			rd.include(request, response);
 			out.println("<script>" + 
 							"document.getElementById(\"beforeDatetime\")" + 
 									".innerHTML=" + 
-										"\"You must submit this form at " + 
-									    "least 1 week before the event.\"" + 
+										"\"Datetime cannot be empty.\"" + 
 						"</script>");
-	    }
-	    
-	    else {
-	    	String s = request.getParameter("cutoff");
-	    	Double cutoff = (Double) null;
-	    	System.out.println(s);
-	    	
-	    	if(s != null) cutoff = Double.valueOf(s);
-	    	
-	    	
-//	    	String grading = request.getParameter("grading");
-			System.out.println(cutoff);
-			String username = employee.getUsername();
-			String type = request.getParameter("type");
-			String location = request.getParameter("location");
-			BigDecimal cost = new BigDecimal(request.getParameter("cost"));
-			String description = request.getParameter("description");
-			String justification = request.getParameter("justification");
-			String timeMissed = request.getParameter("timeMissed") + " 0:0:0.0";
+		}
+		
+		else {
+			SimpleDateFormat sdf = null;
+			Timestamp eventTime = null;
 			
-			Reimbursement reimbursement = 
-					ReimbursementService.submitReimbursement(username, type, 
-															 cost, eventTime, 
-															 location, 
-															 timeMissed, 
-															 cutoff, 
-															 description, 
-															 justification);
+			try {
+			    sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+				eventTime = new Timestamp(sdf.parse(datetime).getTime());
+			} 
 			
-			employee.insertReimbursement(reimbursement.getId(), reimbursement);
-			session.setAttribute("employee", employee);
-			rd = request.getRequestDispatcher("user/index.html");
-			rd.forward(request, response);
-			System.out.println("Please");
-	    }
+			catch (ParseException chrome) {
+				
+				try {
+					sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+					eventTime = new Timestamp(sdf.parse(datetime).getTime());
+				} 
+				
+				catch (ParseException pe) {
+					pe.printStackTrace();
+				}
+			}
+			
+			Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+			Calendar cal = Calendar.getInstance();
+		    cal.setTimeInMillis(currentTime.getTime());
+		    cal.add(Calendar.DAY_OF_MONTH, 7);
+		    Timestamp futureTime = new Timestamp(cal.getTime().getTime());
+	
+		    if (futureTime.compareTo(eventTime) > 0) {
+		    	rd.include(request, response);
+				out.println("<script>" + 
+								"document.getElementById(\"beforeDatetime\")" + 
+									".innerHTML=" + 
+										"\"You must submit this form at" + 
+										"least 1 week before the event.\"" + 
+							"</script>");
+		    }
+		    
+		    else {
+		    	String s = request.getParameter("cutoff");
+		    	Double cutoff = (Double) null;
+		    	
+		    	if(s != null) cutoff = Double.valueOf(s);
+		    	
+	//	    	String grading = request.getParameter("grading");
+				String username = employee.getUsername();
+				String type = request.getParameter("type");
+				String location = request.getParameter("location");
+				BigDecimal cost = new BigDecimal(request.getParameter("cost"));
+				String description = request.getParameter("description");
+				String justification = request.getParameter("justification");
+				String timeMissed = request.getParameter("timeMissed") + 
+									" 0:0:0.0";
+				
+				Reimbursement reimbursement = 
+						ReimbursementService.submitReimbursement(username, 
+																 type, 
+																 cost, 
+																 eventTime, 
+																 location, 
+																 timeMissed, 
+																 cutoff, 
+																 description, 
+																 justification
+																 );
+				
+				employee.insertReimbursement(reimbursement.getId(), 
+											 reimbursement);
+				session.setAttribute("employee", employee);
+				rd = request.getRequestDispatcher("user/index.html");
+				rd.forward(request, response);
+				System.out.println("Please");
+		    }
+		}
 	}
 
 	/**
